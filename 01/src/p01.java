42e722e96f79cdbb2d4650b3dc31cdfb205dc0fa
Planer <science.0071@gmail.com>
2023-08-14 12:47:18 +0330

    test

---
+++ 01/src/p01.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class p01 {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		
+		int n= input.nextInt();
+		
+		
+		for (int x=1; x<=n; x++)
+			System.out.println(x +"" );
+		
+		
+	}
+
+}
